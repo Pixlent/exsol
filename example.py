2f34c17d5123048ece0a3cc3b2bfaf3936b9f6ab
Alexander <2beheima@gmail.com>
2026-09-02 20:34:12 +0200

    feat: add error handling to example.py

--- example.py
+++ example.py
@@ -3,4 +3,5 @@ from main import solve_expression
 user_input = input("Please write an equation: \033[31m")
 answer, err = solve_expression(user_input)
 
-print(f"= {answer}")
+if not answer: print(err)
+else: print(f"= {answer}")
