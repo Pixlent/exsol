ffd658e48aecd8204ff3a66fb7d01a387802b91b
Alexander <91787768+Pixlent@users.noreply.github.com>
2026-09-07 10:06:49 +0200

    docs: add documentation to exsol.py and test.py

--- exsol.py
+++ exsol.py
@@ -3,21 +3,33 @@ from collections.abc import Callable
 from decimal import Decimal, InvalidOperation
 from typing import NamedTuple
 
+# A python script that solves expressions. Based on dijkstra-two-stack algorhitm
+
+# A config of sorts
 SPECIAL_TOKENS = ["(", ")"]
 ALLOW_IDENTIFIERS = False
 
+# Returns true if the string can be interpretet as a number
 def is_valid_number(value: str) -> bool:
     return is_number(value) or value == "."
 
+# Takes an expression as an input and tokenizes it into a list of tokens, returns an error otherwise
+# An example is "-5 * ( 5 + 5 )" into ["-", "5", "*", "(", "5", "+", "5", ")"]
 def tokenize(input: str)-> tuple[list[str] | None, str | None]:
+    # Remove all leading and trailing whitespace
     input = input.strip()
+    # Error if the string is empty
     if not input:
         return None, "The input expression is empty"
 
+    # A buffer used to store numbers, and optionally identifiers
     buffer = ""
+    # This will eventually be the list of tokens
     tokens: list[str] = []
 
+    # Loop over all characters in the expression
     for char in input:
+        # If the character is an operator or parentheses, append it to the list
         if char in OPERATORS or char in SPECIAL_TOKENS:
             if buffer:
                 tokens.append(buffer)
@@ -25,10 +37,13 @@ def tokenize(input: str)-> tuple[list[str] | None, str | None]:
             tokens.append(char)
             continue
 
+        # If we disallow identifiers, return an error if found
         if not ALLOW_IDENTIFIERS:
             if char.isalpha(): return None, "Identifiers are not allowed in expressions"
             if char == "_": continue
 
+        # Check if the character is a number, if so attempt to add it to the buffer
+        # If we can't append the current character to the buffer (it isn't a number), append the buffer to the list
         if is_valid_number(char):
             if is_valid_number(buffer):
                 if "." in buffer and char == ".":
@@ -39,6 +54,7 @@ def tokenize(input: str)-> tuple[list[str] | None, str | None]:
                     tokens.append(buffer)
                 buffer = char
             continue
+        # If identifiers are allowed, we would handle them similar to numbers where they can span multiple characters
         elif char.isalpha() or char == "_":
             if not is_valid_number(buffer):
                 buffer += char
@@ -52,15 +68,18 @@ def tokenize(input: str)-> tuple[list[str] | None, str | None]:
         else:
             return None, f"Unexpected token: {char}"
 
+    # If the buffer isn't empty, append it to the list
     if buffer:
         tokens.append(buffer)
+    # Return the list
     return tokens, None
 
+# The base class for all operands, like addition or multiplication
 class Operator(NamedTuple):
-    precedence: int
-    associativity: str # 'L' or 'R'
-    func: Callable[[list[Decimal]], str | None]
-    arity: int = 2  # 1 for unary, 2 for binary and so on
+    precedence: int                             # The priority of the operator, according to PEMDAS
+    associativity: str                          # 'L' or 'R'
+    func: Callable[[list[Decimal]], str | None] # The function called that does the operation. Returns an error if something failed.
+    arity: int = 2                              # 1 for unary, 2 for binary and so on
 
 def add(stack: list[Decimal]) -> str | None:
     right = stack.pop()
@@ -119,12 +138,15 @@ def fac(stack: list[Decimal]) -> str | None:
     stack.append(Decimal(math.gamma(value + 1)))
     return None
 
+# Negation operator, like in -1
 def neg(stack: list[Decimal]) -> str | None:
     value = stack.pop()
 
     stack.append(value * -1)
     return None
 
+# The list of all operators, their priority (according to pemdas), whether or not they're left or right associated,
+# The function that actually applies the operation to the stack and optionally how many numbers the operation takes.
 OPERATORS: dict[str, Operator] = {
     "+": Operator(precedence=1, associativity="L", func=add),
     "-": Operator(precedence=1, associativity="L", func=sub),
@@ -136,6 +158,7 @@ OPERATORS: dict[str, Operator] = {
     "_neg": Operator(precedence=3, associativity="R", func=neg, arity=1)
 }
 
+# Returns true if the string can be cast into the Decimal class (a number)
 def is_number(value: str) -> bool:
     try:
         _ = Decimal(value)
@@ -143,13 +166,19 @@ def is_number(value: str) -> bool:
     except (InvalidOperation, TypeError):
         return False
 
+# Takes in a list of tokens, for instance: ["-", "5", "*","(", "5", "+", "5", ")"]
+# And tries to evaluate it into a single number.
+# Returns the number if successful and an error otherwise.
 def eval_expression(tokens: list[str]) -> tuple[Decimal | None, str | None]:
+    # The value stack
     out: list[Decimal] = []
+    # The operator stack
     ops: list[str] = []
 
     if tokens.count("(") != tokens.count(")"):
         return None, "Mismatched parentheses: expected a matching parenthesis"
 
+    # If the expression contains a minus, check for the negation operator
     if "-" in tokens:
         for index, token in enumerate(reversed(tokens)):
             if token == "-":
@@ -159,6 +188,7 @@ def eval_expression(tokens: list[str]) -> tuple[Decimal | None, str | None]:
                 if tokens[len(tokens)-2-index] in OPERATORS or tokens[len(tokens)-2-index] == "(":
                     tokens[(len(tokens)-1)-index] = "_neg"
 
+    # If the expression contains a plus, mark it for removal
     if "+" in tokens:
         for index, token in enumerate(reversed(tokens)):
             if token == "+":
@@ -167,24 +197,33 @@ def eval_expression(tokens: list[str]) -> tuple[Decimal | None, str | None]:
                     continue
                 if tokens[len(tokens)-2-index] in OPERATORS or tokens[len(tokens)-2-index] == "(":
                     tokens[(len(tokens)-1)-index] = "_rem"
+    # Remove everything marked for removal
     tokens[:] = [token for token in tokens if token != "_rem"]
 
+    # Solve the expression using dijkstra-two-stack algorhitm, start by looping over all tokens
     for token in tokens:
+        # If the token is a number, push it to the number stack
         if is_number(token):
             out.append(Decimal(token))
             continue
 
+        # If the token is a open parentheses, push it to the operator stack
         if token == "(": ops.append("(")
+        # If the token is a closing parentheses, loop over the operator stack until we find a open parentheses
         if token == ")":
             for _ in range(len(ops)):
                 op = ops.pop()
+                # If we find a open parentheses, exit
                 if op == "(":
                     break
+                # Pop the operator and run it
                 err = OPERATORS[op].func(out)
                 if err:
                     return None, err
 
+        # Do the following logic if the token is an operator
         if token in OPERATORS:
+            # If the operator is an open parentheses, add it to the operator stack
             if len(ops) < 1 or ops[len(ops) - 1] == "(":
                 ops.append(token)
                 continue
@@ -193,16 +232,21 @@ def eval_expression(tokens: list[str]) -> tuple[Decimal | None, str | None]:
             token_associativity = OPERATORS[token].associativity
             stack_precedence = OPERATORS[ops[len(ops) - 1]].precedence
 
+            # If the operator is left associated and has higher precedence than the first operator on the operator stack, append it
+            # If the operator is right associated and has a higher or equal precedence than the first operator on the operator stack, append it
             if (token_associativity == "L" and token_precedence > stack_precedence) or (token_associativity == "R" and token_precedence >= stack_precedence):
                 ops.append(token)
                 continue
             if len(out) < OPERATORS[ops[len(ops) - 1]].arity:
                 return None, "Not a valid expression: too many operators or not enough numbers"
+            # Pop the operator and execute it
             err = OPERATORS[ops.pop()].func(out)
             if err:
                 return None, err
+            # Append the operator
             ops.append(token)
 
+    # Loop over the remaining operators from left to right and pop them
     for op in reversed(ops):
         if op == "(" or op == ")":
             return None, "Mismatched parentheses: expected a matching parenthesis"
@@ -212,13 +256,17 @@ def eval_expression(tokens: list[str]) -> tuple[Decimal | None, str | None]:
         if err:
             return None, err
     if len(out) != 1: return None, "Failed to evaluate expression, less or more than one remaining result"
+    # Return the number
     return out[0], None
 
 def solve_expression(expression: str) -> tuple[Decimal | None, str | None]:
+    # Attempt to tokenize the string, exit early if we encounter problems
     tokens, lex_error  = tokenize(expression)
     if not tokens:
         return None, lex_error
+    # Attempt to solve the expression, exit early if we encounter problems
     result, eval_error = eval_expression(tokens)
     if result is None:
         return None, eval_error
+    # Return the result if everything went accordingly
     return result, None
--- test.py
+++ test.py
@@ -3,11 +3,13 @@ from decimal import Decimal, InvalidOperation
 
 from exsol import solve_expression
 
+# A config of sorts
 SOLVE_EXPRESSION = solve_expression
 TEST_FILE = "tests.txt"
 PERCENT_DECIMAL = 2
 PRINT_SUCCESSFUL_TESTS = False
 
+# A table of colors, usage: Use a pretty string (f"") and use {} with the property to insert it
 RESET        = "\033[0m"
 BOLD         = "\033[1m"
 
@@ -25,12 +27,15 @@ BACK_BLUE    = "\033[44m"
 BACK_MAGENTA = "\033[45m"
 BACK_CYAN    = "\033[46m"
 
+# The unit for a test, contains a description of what the test is,
+# the expression being tested and the correct answer
 @dataclass
 class Test:
     description: str
     expression: str
     answer: tuple[Decimal | None, str | None]
 
+# Returns true if the string can be cast to the Decimal class
 def is_number(value: str) -> bool:
     try:
         _ = Decimal(value)
@@ -38,29 +43,41 @@ def is_number(value: str) -> bool:
     except (InvalidOperation, TypeError):
         return False
 
+# Returns a parsed list of tests, loaded from a text file
 def import_list() -> list[Test]:
+    # Read the file
     with open(TEST_FILE, "r", encoding="utf-8") as file:
+        # Initialize the array that contains the tests
         tests: list[Test] = []
+        # An array of the lines in the file
         raw_tests = file.readlines()
+        # The description of the current test being read
         description = "Blank"
 
+        # Loop over all lines in the file
         for line in raw_tests:
+            # Skip if the line doesn't contain an equal sign
             if not "=" in line:
                 continue
 
+            # Parse the description
             if line.startswith("[") and "]" in line:
                 end_bracket = line.find("]")
                 description = line[1:end_bracket]
                 line = line[end_bracket + 1:].strip()
 
+            # Extract the expression and the answer from the equation
             expression, answer_str = (part.strip() for part in line.rsplit("=", 1))
 
+            # If the answer is a valid number, parse it
             if is_number(answer_str): answer = (Decimal(answer_str), None)
             else: answer = (None, answer_str)
 
+            # Append the test
             tests.append(Test(description, expression, answer))
     return tests
 
+# Parse the percent between two numbers and return it in a pretty manner
 def pretty_percent(part: int, whole: int) -> str:
     part *= 100
 
@@ -68,7 +85,8 @@ def pretty_percent(part: int, whole: int) -> str:
         return f"~{round(part / whole, PERCENT_DECIMAL)}"
     return f"{round(part / whole)}"
 
-def pretty_index(num: int, length: int):
+# Return a pretty index where we have enough space to so the indexes align when printed
+def pretty_index(num: int, length: int) -> str:
     pretty = str(num)
 
     while len(pretty) != length:
@@ -76,34 +94,43 @@ def pretty_index(num: int, length: int):
     return pretty
 
 
-# Returns true if the check passes, false otherwise. bool, answer, err
+# Returns true if the test passes, false otherwise. bool: true if the test passes, answer: the correct answer, err: the error encountered if it doesn't succeed
 def check_answer(test: Test) -> tuple[bool, Decimal | None, str | None]:
     try:
+        # Try to solve the expression using the solver provided
         result, err = SOLVE_EXPRESSION(test.expression)
 
         if err and err == test.answer[1]: return True, None, err # Passed, there is supposed to be an error, and they match
-        if result == None: return False, None, err                   # Not passed, there's no result, therefore an error
+        if result == None: return False, None, err               # Not passed, there's no result, therefore an error
         return result == test.answer[0], result, None            # Passes if the result matches the key answer
+    # Catch exceptions thrown by python: for instance if the program crashes
     except Exception as e:
         err = str(e)
 
         if err: return False, None, err
         else: return False, None, "Unknown exception"
 
+# Runs the tests and print the results
 def test():
+    # Import the list of tests
     tests = import_list()
 
+    # Calculate the total amount of tests
     total_tests = len(tests)
+    # Keep count of how many tests pass
     tests_passed = 0
 
+    # Loop over all tests
     for index, test in enumerate(tests):
         test_number = pretty_index(index + 1, len(str(total_tests)))
         passes, result, err = check_answer(test)
 
+        # If the test passes, increment the counter and print the test if enabled
         if passes:
             tests_passed += 1
             if PRINT_SUCCESSFUL_TESTS:
                 print(f"[{test_number}. Test passed] {TEXT_YELLOW}{test.expression}{RESET} = {TEXT_GREEN}{test.answer[0]}{RESET}")
+        # If the test fails, print as much relevant information and possible
         else:
             print(f"{BACK_RED}[{test_number}. Test failed]{RESET}")
             print(f"expression . . . | {TEXT_YELLOW}{test.expression}{RESET} != {TEXT_YELLOW}{test.answer[0]}{RESET}")
@@ -111,12 +138,15 @@ def test():
             print(f"solver . . . . . | {TEXT_RED}{result}{RESET}")
             print(f"error  . . . . . | {TEXT_RED}{err}{RESET}")
 
+    # Calculate the percent passed in a pretty manner
     percent_passed = pretty_percent(tests_passed, total_tests)
 
+    # Print results to the console
     print()
     if total_tests == tests_passed:
         print(f"All tests passed {TEXT_GREEN}(100%)")
     else:
         print(f"{TEXT_YELLOW}{tests_passed} of {TEXT_YELLOW}{total_tests} tests passed {TEXT_RED}({percent_passed}%)")
 
+# Run all the tests
 test()
